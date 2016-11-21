27369ed8a0ac5e22e5ce15ecaaedec3861db85bf
Natalia Hess <nathess91@gmail.com>
2016-11-21 09:34:53 -0800

    add enum to assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,9 @@
 class Assignment < ApplicationRecord
   belongs_to :user
+
+  enum loss_category: [:water, :burglary_theft, :vandalism, :small_fire, :large_fire, :impact, :remodel, :hail, :vehicle]
+  enum estimator_experience: [:good, :bad, :questionable]
+  enum assignment_stage: [:assigned, :approved, :in_progress, :not_sold, :completed, :closed]
+  enum lost_sales_related: [:cashed_out, :declined, :own_contractor]
+  enum lost_other: [:below_deductible, :not_covered, :other]
 end
