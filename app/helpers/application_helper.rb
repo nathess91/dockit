d6951ad79c82388f77b5f4486e6fc447fa317716
Natalia Hess <nathess91@gmail.com>
2016-11-28 16:13:40 -0800

    delete unnecessary helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
 		return AssignmentNote.where(assignment_id: assignment_id)
 	end
 
-  def get_assignment_contact(assignment_id)
-    return AssignmentContact.where(assignment_id: assignment_id)
-  end
+  def get_assignment_contacts(assignment_id)
+		return AssignmentContact.where(assignment_id: assignment_id)
+	end
 
 end
